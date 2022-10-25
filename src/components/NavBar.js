ac3799e229aa47cd7f290fee6f6b506b2dc18dd2
Lucas <lucasromano96@gmail.com>
2022-10-25 14:27:11 -0300

    navbar-desafio

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import CartWidget from "./CartWidget";
+
+const NavBar = () =>{
+    return(
+        <header>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Manga.Shop</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav navbar-centrado">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Shonen</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Seinen</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Gore</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <CartWidget />
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar;
